236f9f91a6a2295fe909714ba2b16d323a448422
diamond-nicholas <diamondnicholas154@gmail.com>
2022-03-06 09:12:30 +0100

    connect db, add packages, connect port, host db

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const connectDB = require('./db/connect');
+require('express-async-errors'); //async errors
+
+//app config
+app.use(express.json());
+const PORT = process.env.PORT || 8001;
+
+//middlewares
+
+//api endpoints
+app.get('/', (req, res) => {
+  res.status(200).send('hello world and yey my app is running live');
+});
+
+//listener
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`app is listening on port ${PORT}....`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
