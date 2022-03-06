b7ceb4ae0f1c4f9d600cff1e4ecba1e2d1f53a91
diamond-nicholas <diamondnicholas154@gmail.com>
2022-03-06 15:19:22 +0100

    add users model for registration

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  username: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model('Users', userSchema);
